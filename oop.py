dd753b4b57e6caa1cc896e9375fab6b1d6c36be7
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2024-02-13 22:55:10 +0200

    Add files via upload
    uploaded first file

---
+++ oop.py
@@ -0,0 +1,17 @@
+import json
+
+class InputPrework:
+    def __init__(self, input):
+        self.input = input
+
+class PCCS:
+    def __init__(self, input):
+        self.input = input
+
+class FloatToInt:
+    def __init__(self, input):
+        self.input = input
+
+class CIPS:
+    def __init__(self, input):
+        self.input = input
