278f8ba1749d1ef937e0a4e4135ad508e7e5905d
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2024-02-14 09:52:09 +0200

    Zīverts atļāva paņemt skolas datoru :)

--- oop.py
+++ oop.py
@@ -5,14 +5,34 @@ class InputPrework:
                    Tas iekļauj sevī saraksta _(list)_ lasīšanu un datu definēšanu."
     def __init__(self, input):
         self.input = input
-        self.inputClass = str(type(input))
+        self.inputClass = ""
 
-    def NotIntOrFloat(text, symbols):
-        state = False
-        for char in text:
-            if char not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-                state = True
-        return state
+    def NotInt(self):
+        notInt = False
+        for char in self.input:
+            if char is ".":
+                notInt = True
+        return notInt
+
+    def NotFloat(self):
+        notFloat = False
+        for char in self.input:
+            if char not in ["-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
+                notFloat = True
+        return notFloat
+
+    def SetDataClass(self):
+        if NotFloat() == True    and NotInt == True :
+            self.inputClass = "STRING"
+        
+        elif NotFloat() == True  and NotInt == False:
+            self.inputClass = "FLOAT"
+            self.input = float(self.input)
+        
+        elif NotFloat() == False and NotInt == False:
+            self.inputClass = "INTEGER"
+            self.input = int(self.input)
+
 
     def __repr__(self):
         return self.input
@@ -26,14 +46,21 @@ class PointPerCharacterConvertingSystem:
         self.input = input
 
 class FloatToInt:
-    description = ""
+    description = "Šī klase pārveido daļu _(float)_ par skaitli _(integer)_"
     def __init__(self, input):
-        self.input = input
+        self.input = int(str(input).replace(".", ""))
+
+    def __repr__(self):
+        return self.input
     
 class ComplexIntegerProcessingScript:
-    description = ""
+    description = "Šī klase apstrādā skaitļus _(integer)_ ar visdažādākām matemātiskām metodēm 100 reizes"
+    decrementBase = 100
     def __init__(self, input):
         self.input = input
+
+    def __repr__(self):
+        return self.input
 
 class Processing:
     description = "Šī klase ir atbildīga par visu datu apstrādi."
@@ -41,6 +68,12 @@ class Processing:
         self.list = input
         for unit in input:
             self.dataUnit = input[unit]
-            PirmaisPosms = InputPrework(self.dataUnit)
-            
-
+            self.firstStage = InputPrework(self.dataUnit)
+            if firstStage.ReturnClass() == "STRING":
+                self.secondStage = PointPerCharacterConvertingSystem(firstStage)
+                self.thirdStage = ComplexIntegerProcessingScript(secondStage)
+            elif firstStage.ReturnClass() == "FLOAT":
+                self.secondStage = FloatToInt(firstStage)
+                self.thirdStage = ComplexIntegerProcessingScript(secondStage)
+            else:
+                self.thirdStage = ComplexIntegerProcessingScript(firstStage)
