b9ef5a599f0ee5de6e4a344e37ed45d24ae31eb9
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2024-02-15 11:20:14 +0200

    Kolhozoju no telefona

--- dekl.py
+++ dekl.py
@@ -1,1 +1,22 @@
- 
+import json
+with open('input.json', 'r') as file: list = json.load(file)
+
+for item_raw in list: 
+    item = str(item_raw)
+    notInt = False
+    notFloat = False
+    for char in item:
+        inInt = ["-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        if char not in inInt:
+            notFloat = True
+        if char == ".":
+            notInt = True
+    if notFloat == False and notInt == True:
+        itemClass = "FLOAT"
+        item = float(item_raw)
+    elif notFloat == False and notInt == False:
+        itemClass = "INTEGER"
+        item = int(item_raw)
+    else: 
+        itemClass = "STRING"
+    print(f"{item1} is {itemClass}")
